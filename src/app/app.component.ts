f016d62768ad809a7617ccb10b7588a14aab8d29
Herve Ekem <ekemherve@gmail.com>
2018-08-14 12:20:14 +0200

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'MonProjetAgular-Hello';
+  title = 'Buisness Training Angular Course';
 }
